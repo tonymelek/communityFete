fda74f4aed1b028b70e8a83593fdb8e4a9d4bbea
Tony Melek <tonymelek.au@gmail.com>
2021-01-09 12:25:54 +1100

    create schema.sql
    main Schema to creare db

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "communityFete_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- Use the following 3 lines to create database on workbench
+drop database if exists communityFete_db;
+create database communityFete_db;
+use communityFete_db;
