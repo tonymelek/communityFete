ee0d8df94eb8cb4eb9734b93f2d466300a637f87
Tony Melek <tonymelek.au@gmail.com>
2021-02-01 23:06:18 +1100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 module.exports = {
-  // "development": {
-  //   "username": process.env.DB_USER,
-  //   "password": process.env.DB_PASS,
-  //   "database": "communityFete_db",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "communityFete_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
   "production": {
-    "database": "JAWSDB_URL",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
